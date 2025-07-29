fef27557cff22d9fec6625c2f1341e6a6abad2e1
A-bacate <giovanna.c.magagnin@gmail.com>
2025-07-29 09:35:55 -0300

    exercícios

---
+++ Exercicio1G.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")();
+
+let num = Number(prompt("Digite um número: "));
+
+if (num%3==0) {
+    console.log("É múltiplo de três!");
+} else {
+    console.log("Não é múltiplo de três!");
+}
+
+/*
+Escreva um algoritmo que receba um número e imprima uma das seguintes mensagens: “É
+múltiplo de três” ou “Não é múltiplo de três”.
+*/
