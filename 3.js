938cd16709dbf91b308a09fa9dc4c49a5af3405e
A-bacate <giovanna.c.magagnin@gmail.com>
2025-08-04 11:33:59 -0300

    programação

--- 3.js
+++ 3.js
@@ -81,3 +81,37 @@ console.log(`\n\n\t--> Pessoa mais idosa:\n\t\t--> ${nome[c]}\n\t\t--> ${maior} 
 console.log(`\n\n\t--> Pessoa mais jovem:\n\t\t--> ${nome[d]}\n\t\t--> ${menor} anos`);
 
 */
+
+
+
+/*
+e) Crie um algoritmo que leia vários números e encerre a leitura com 0 (zero). Após o
+encerramento, deve-se imprimir o maior, o menor e a média aritmética dos números. O número 0 (zero)
+não faz parte da sequência.
+
+*/
+let n = [], k = 0, continuar = true;
+let maior, menor, soma;
+console.log(`\n\tDigite números (0 para encerrar):`);
+while(continuar) {
+    n[k]= Number(prompt(`\n\t--> `));
+    if (n[k]==0) {
+        continuar == false;
+    }
+    ++k;
+    soma += n[k]
+}
+menor = n[0];
+maior = n[0];
+for (let j=0;j<k;j++){
+    if (maior <= n[j]){
+        maior = n[j];
+    }
+    if (menor >= n[j]){
+        menor = n[j];
+    }
+}
+console.log(`\n\t--> Maior número: ${maior}`);
+console.log(`\n\t--> Menor número: ${maior}`);
+console.log(`\n\t--> Média: ${soma/k}`);
+
