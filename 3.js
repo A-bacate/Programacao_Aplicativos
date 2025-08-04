0a63f21a5450b76a3f3cb2a0e00432776bfd1c17
A-bacate <giovanna.c.magagnin@gmail.com>
2025-08-04 10:23:19 -0300

    programação

--- 3.js
+++ 3.js
@@ -52,5 +52,10 @@ console.log(`Média salarial: ${media}`);
 
 
 /*
+d) Criar um algoritmo para ler o nome e a idade de um grupo de pessoas, sendo o número desse
+grupo determinado pelo usuário. Ao final, o programa deverá imprimir o nome e a idade da pessoa mais
+idosa e o nome e a idade da pessoa mais jovem.
+
+
 
 */
