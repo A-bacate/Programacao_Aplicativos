ec02a54d69b31770398a6ca85e34aebd9334229d
A-bacate <giovanna.c.magagnin@gmail.com>
2025-08-04 10:22:52 -0300

    programação

---
+++ 3.js
@@ -0,0 +1,56 @@
+const prompt = require("prompt-sync")();
+
+/*
+
+a) Crie um programa que simule 10 batalhas de um personagem. Use um laço for para exibir no
+console a seguinte mensagem a cada batalha: Batalha 1: Você ganhou 10 pontos de experiência! Ao
+final exibir o total de experiência recebida.
+
+let total=0;
+for (let i=1;i<=10;i++){
+    console.log(`\nBatalha ${i}: Você ganhou 10 pontos de experiência!`);
+    total += 10;
+}
+console.log(`\nTotal de experiência: ${total}`);
+
+*/
+
+
+
+/*
+
+b) Faça um algoritmo que apresente na tela as tabuadas de 2 a 9.
+
+for (let i=0;i<=10;i++) {
+    console.log(`\nTabuada do ${i}: `);
+    for (let j=0;j<=10;j++) {
+        console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
+
+*/
+
+
+
+/*
+c) Escreva um algoritmo que leia o código e o salário de 20 funcionários de uma empresa. Ao final,
+o algoritmo deve determinar o total da folha de pagamento da empresa e sua média salarial.
+
+let salario=0, media=0, soma=0;
+let codigo; //aqui ficam os inuteis :)
+for (let i=1;i<=20;i++) {
+    codigo = prompt(`\n\t--> Código: `);
+    salario = Number(prompt(`\t--> Salário: `));
+    soma += salario;
+}
+media = soma/20;
+console.log(`\nFolha de pagamento: ${soma}`);
+console.log(`Média salarial: ${media}`);
+
+*/
+
+
+
+/*
+
+*/
