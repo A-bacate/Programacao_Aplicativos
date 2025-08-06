2d9f38e851f96b4b430638466b7751924e913660
A-bacate <giovanna.c.magagnin@gmail.com>
2025-08-06 11:18:45 -0300

    prog

--- 4.js
+++ 4.js
@@ -103,13 +103,15 @@ console.log(vetor);
 /*
 f) Dada uma sequência numérica de 30 elementos armazenados num vetor, determinar o índice
 (posição) do maior elemento do conjunto. Suponha que os elementos sejam distintos.
-*/
-let numeros = [27, 5, 14, 21, 9, 29, 1, 18, 30, 25, 11, 28, 7, 23, 16, 4, 19, 13, 26, 8, 3, 22, 6, 17, 10, 24, 15, 20, 12, 2];
-let maior, index;
 
-for (let j=0;j<5;j++){
+let numeros = [27, 5, 14, 21, 9, 29, 1, 18, 30, 25, 11, 28, 7, 23, 16, 4, 19, 13, 26, 8, 3, 22, 6, 17, 10, 24, 15, 20, 12, 2];
+let maior=0, index=0;
+
+for (let j=0;j<30;j++){
     if (maior <= numeros[j]){
         maior = numeros[j];
     }
 }
-index = numeros.indexOf
+index = numeros.indexOf(maior);
+console.log(index);
+*/
