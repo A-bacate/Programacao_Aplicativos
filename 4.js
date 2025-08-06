37fc2492b5ee7c2466ca0558bf75c75a05c81478
A-bacate <giovanna.c.magagnin@gmail.com>
2025-08-06 11:12:07 -0300

    prog

--- 4.js
+++ 4.js
@@ -1,16 +1,13 @@
 const prompt = require("prompt-sync")();
 
-/*
-.pop()
-.unshift()
-.shift()
-.length
-.indexOf()
-.sort()
-.reverse()
-math.random(x,y)
-
-*/
+// .pop()
+// .unshift()
+// .shift()
+// .length
+// .indexOf()
+// .sort()
+// .reverse()
+// math.random(x,y)
 
 /*
 a) Crie um array chamado jogos com 5 jogos. Em seguida, adicione mais um jogo ao final do array.
@@ -85,4 +82,34 @@ let soma = Array[15]; soma = [0];
 e) Faça um algoritmo que armazene 30 valores reais em um vetor. Depois, modifique o vetor de
 modo que os valores das posições ímpares sejam aumentados em 5%, e os das posições pares sejam
 aumentados em 2%. Imprima o vetor resultante.
+
+let vetor = [45.78,12.31,29.05,3.56,19.99,33.82,48.14,8.77,21.43,41.60,
+            5.92,36.25,17.08,2.49,49.33,26.51,10.04,44.91,30.15,7.28,
+            23.76,38.47,14.80,47.03,1.62,40.55,32.90,6.18,25.39,9.85];
+
+console.log(vetor);
+for (let i=0;i<30;i++){
+    if (vetor[i]%2==0){
+        vetor[i] += vetor[i]*0.02;
+    } else {
+        vetor[i] += vetor[i]*0.05;
+    }
+    vetor[i]=Number(vetor[i].toFixed(2));
+}
+console.log(`\n\nPosições ímpares aumentadas em 5% e posições pares aumentadas em 2%:`);
+console.log(vetor);
 */
+
+/*
+f) Dada uma sequência numérica de 30 elementos armazenados num vetor, determinar o índice
+(posição) do maior elemento do conjunto. Suponha que os elementos sejam distintos.
+*/
+let numeros = [27, 5, 14, 21, 9, 29, 1, 18, 30, 25, 11, 28, 7, 23, 16, 4, 19, 13, 26, 8, 3, 22, 6, 17, 10, 24, 15, 20, 12, 2];
+let maior, index;
+
+for (let j=0;j<5;j++){
+    if (maior <= numeros[j]){
+        maior = numeros[j];
+    }
+}
+index = numeros.indexOf
--- 4c.js
+++ 4c.js
@@ -2,16 +2,16 @@
 const prompt = require("prompt-sync")();
 
 let notas = Array(35);
-for (let j=0;j<5;j++){
+for (let j=0;j<35;j++){
     notas[j] = Array(2);
 }
 
-for (let i=0;i<5;i++){
+for (let i=0;i<35;i++){
     notas[i][0] = (prompt(`Digite o nome do Aluno: `));
     notas[i][1] = Number(prompt(`Digite a nota: `));
 }
 console.log(`\n\nNotas iguais ou superiores a 7.0:\n`);
-for (i=0;i<5;i++){
+for (i=0;i<35;i++){
     if (notas[i][1]>=7){
         console.log(`O aluno ${notas[i][0]} tem ${notas[i][1]} de nota`);
     }
