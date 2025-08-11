0e6d15f3e46ea4efd9f34660ada8548d4a57eed0
Your Name <you@example.com>
2025-08-11 11:01:41 -0300

    a

--- funcoes.js
+++ funcoes.js
@@ -1,17 +1,31 @@
 //declaração
-function mostrarMensagem(nome){
-    console.log(`Olá ${nome}`);
-}
+// function mostrarMensagem(nome){
+//     console.log(`Olá ${nome}`);
+// }
 
 //função (possui retorno)
 
 //chamada
-let resultado = somar(3,8);
-console.log(resultado);
+// let resultado = somar(3,8);
+// console.log(resultado);
 
 console.log(somar(3,8));
 
 //declaração
 function somar(num1,num2){
     return num1+num2;
+}
+let notas = [7,5.5,8.5,10,2.5];
+console.log(calcularMedia(notas));
+
+function calcularMedia(numeros){
+    if (numeros.length <= 0){
+        return 0;
+    }
+    let soma = 0;
+    
+    numeros.forEach(element => {
+        soma += element;
+    });
+    return soma / notas.length;
 }
