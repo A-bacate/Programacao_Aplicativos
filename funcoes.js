1a5a13c5a3ec667965aefba9aabf17c9197943bf
Your Name <you@example.com>
2025-08-11 10:56:14 -0300

    a

---
+++ funcoes.js
@@ -0,0 +1,17 @@
+//declaração
+function mostrarMensagem(nome){
+    console.log(`Olá ${nome}`);
+}
+
+//função (possui retorno)
+
+//chamada
+let resultado = somar(3,8);
+console.log(resultado);
+
+console.log(somar(3,8));
+
+//declaração
+function somar(num1,num2){
+    return num1+num2;
+}
