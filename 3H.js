9f1b842a36e284194fa8e0268d1dda66f916dd6a
A-bacate <giovanna.c.magagnin@gmail.com>
2025-09-02 18:30:06 -0300

    programação

--- 3H.js
+++ 3H.js
@@ -6,7 +6,34 @@ const prompt = require("prompt-sync")();
 // digita o número da conta igual a 0 (zero).
 
 let fim = false;
+let contador = 0;
 let conta = [];
+let saldo = [];
+let negativo = 0;
+
+console.log('');
 while (!fim) {
-    
+    conta[contador] = Number(prompt("Número da conta [0 para encerrar] --> "));
+    if (conta[contador]==0){
+        fim=true
+        break;
+    } else {
+        saldo[contador] = Number(prompt('Saldo --> '));
+        if (saldo[contador]<0){
+            console.log('\nNegativo!\n');
+            negativo++;
+        } else if (saldo[contador]==0){
+            console.log('\nZero!\n');
+        } else {
+            console.log('\nPositivo!\n');
+        }
+        contador++
+    }
 }
+
+console.log('\n');
+for (let i=0;i<contador;i++){
+    console.log(`--> ${conta[i]} - Saldo: ${saldo[i]}`);
+}
+console.log(`\n${negativo} pessoas têm saldo negativo!`);
+
